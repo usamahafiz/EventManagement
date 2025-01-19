3cacaa39fe75a8128fff331cdf251aa8779a65d5
Hafiz Usama <160656348+usamahafiz@users.noreply.github.com>
2025-01-19 22:32:41 +0500

    Update firebase.js

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -5,13 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDCuU7pAIRVcgYfpK2yQLTN7HNu2IOmTGs",
-  authDomain: "todo-with-firebase-7.firebaseapp.com",
-  projectId: "todo-with-firebase-7",
-  storageBucket: "todo-with-firebase-7.appspot.com",
-  messagingSenderId: "919987213554",
-  appId: "1:919987213554:web:78ae78af9074641cd1ac6b",
-  measurementId: "G-GVVCM8G69M",
+
 };
 
 // Initialize Firebase App
