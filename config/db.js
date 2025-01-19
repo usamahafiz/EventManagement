5a1e54a0e5dcfa7337f9116c7cd0ffcdd1b8e357
Hafiz Usama <160656348+usamahafiz@users.noreply.github.com>
2025-01-19 22:30:25 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     // Replace <db_password> with your actual MongoDB password
     const conn = await mongoose.connect(
-      "mongodb+srv://hafizmuhammadusama664:HafizUsama@cluster0.9zg3m.mongodb.net/",
+      "MONGODB_URL",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
